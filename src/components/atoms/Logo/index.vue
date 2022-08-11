98370d4d06adec562c251ec489115dcd9d427332
Stefano Mitello <stefano@mono.studio>
2022-08-11 18:52:01 +0200

    create logo component

---
+++ src/components/atoms/Logo/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo">
+    <ImageWrapper
+      src="https://upload.wikimedia.org/wikipedia/commons/9/96/SpaceX_Logo_Black.png"
+      alt="logo"
+      :class="[{ invert: props.white }]"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import ImageWrapper from "../ImageWrapper/index.vue";
+
+export interface Props {
+  white: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
