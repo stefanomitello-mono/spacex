7e88e99f9037c700bd91dc8219a0f712e77a48f3
Stefano Mitello <stefano@mono.studio>
2022-08-12 13:09:18 +0200

    added object data

--- src/components/organisms/Hero/index.vue
+++ src/components/organisms/Hero/index.vue
@@ -14,21 +14,11 @@
     <div class="container relative mx-auto flex items-center justify-center">
       <ImageWrapper
         class="absolute -z-1 max-w-lg hidden md:block"
-        custom-class="object-cover w-full h-full"
-        src="https://astro-moon-landing.netlify.app/assets/images/astronaut/yLnzHhqALK-800.avif"
-        alt="A floating astronaut in a space suit"
+        :image="astro"
       />
 
       <div class="flex flex-col max-w-xs md:max-w-lg">
         <Logo white />
-        <!--         <h2
-          class="font-extrabold tracking-tighter text-center text-8xl text-white"
-        >
-          Build fast
-          <br />
-          websites, <br />
-          faster.
-        </h2> -->
       </div>
 
       <ScrollButton sectionId="#about" />
@@ -41,6 +31,12 @@ import ImageWrapper from "../../atoms/ImageWrapper/index.vue";
 import VideoWrapper from "../../atoms/VideoWrapper/index.vue";
 import Logo from "../../atoms/Logo/index.vue";
 import ScrollButton from "../../atoms/ScrollButton/index.vue";
+
+const astro = {
+  src: "https://astro-moon-landing.netlify.app/assets/images/astronaut/yLnzHhqALK-800.avif",
+  alt: "A floating astronaut in a space suit",
+  customClass: "object-cover w-full h-full",
+};
 </script>
 
 <style scoped></style>
