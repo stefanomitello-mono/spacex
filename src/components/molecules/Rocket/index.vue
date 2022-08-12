87a9ede4070c4dd155bbc3faa238352fc4893ab6
Stefano Mitello <stefano@mono.studio>
2022-08-12 16:33:09 +0200

    replace button

--- src/components/molecules/Rocket/index.vue
+++ src/components/molecules/Rocket/index.vue
@@ -8,21 +8,7 @@
           {{ description }}
         </p>
         <span class="inline-flex">
-          <a class="text-gray-700" :href="wikipedia">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
-              ></path>
-            </svg>
-          </a>
+          <Button :to="wikipedia" text="Discover" />
         </span>
       </div>
     </div>
@@ -32,6 +18,7 @@
 <script setup lang="ts">
 import ImageWrapper from "../../atoms/ImageWrapper/index.vue";
 import { IImage } from "../../atoms/ImageWrapper/types";
+import Button from "../../atoms/Button/index.vue";
 export interface Props {
   id: string;
   first_flight: string;
