0f6afaae2ce8761f5823b852990c817be7ebb311
Stefano Mitello <stefano@mono.studio>
2022-08-12 16:32:50 +0200

    created button

---
+++ src/components/atoms/Button/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <a
+    :href="to"
+    class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  to: string;
+  text: string;
+}
+
+defineProps<Props>();
+</script>
