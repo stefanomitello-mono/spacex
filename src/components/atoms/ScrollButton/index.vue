8a8c426f6da7c107992bea30481a636c84ece33f
Stefano Mitello <stefano@mono.studio>
2022-08-12 16:27:02 +0200

    update path

--- src/components/atoms/ScrollButton/index.vue
+++ src/components/atoms/ScrollButton/index.vue
@@ -3,7 +3,7 @@
     class="animate-bounce text-12 absolute bottom-6 cursor-pointer w-10 h-10 left-[50%]"
     :href="`#${sectionId || ''}`"
   >
-    <img src="icons/drag-down.svg" /> {{ label }}
+    <img src="/icons/drag-down.svg" /> {{ label }}
   </a>
 </template>
 
