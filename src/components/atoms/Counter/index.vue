37e24b6d0b3504f999a360f4a74a0bcb815c3bd8
Stefano Mitello <stefano@mono.studio>
2022-08-12 13:08:08 +0200

    create counter

---
+++ src/components/atoms/Counter/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4 sm:w-1/4 w-1/2">
+    <h2 class="title-font font-medium sm:text-4xl text-3xl text-white">
+      {{ prefix }}{{ to }}{{ suffix }}
+    </h2>
+    <p class="leading-relaxed">{{ label }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  to: number;
+  prefix?: string;
+  suffix?: string;
+  label: string;
+}
+defineProps<Props>();
+</script>
