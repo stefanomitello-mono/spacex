2e34caeddeba1ff8555c75775bb480a7f63dfbc7
Stefano Mitello <stefano@mono.studio>
2022-08-12 13:08:37 +0200

    export interface on imagewrapper

--- src/components/atoms/ImageWrapper/index.vue
+++ src/components/atoms/ImageWrapper/index.vue
@@ -3,19 +3,14 @@
     <!--         <source v-if="mobile" media="(max-width: 599px)" :srcset="transformUrl(mobile.url)" />
         <source media="(min-width: 600px)" :srcset="transformUrl(data?.url)" /> -->
 
-    <img :class="[customClass]" :src="src" :alt="alt" />
+    <img :class="[image.customClass]" :src="image.src" :alt="image.alt" />
   </picture>
 </template>
 
 <script lang="ts" setup>
+import { IImage } from "./types";
 export interface Props {
-  customClass?: string;
-  alt?: string;
-  caption?: string;
-  width?: number;
-  height?: number;
-
-  src: string;
+  image: IImage;
 }
 defineProps<Props>();
 </script>
---
+++ src/components/atoms/ImageWrapper/types.ts
@@ -0,0 +1,9 @@
+export interface IImage {
+  customClass?: string;
+  alt?: string;
+  caption?: string;
+  width?: number;
+  height?: number;
+
+  src: string;
+}
