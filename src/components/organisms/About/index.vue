dd7776cb0010fdaae9a43c05de66c92e22b38939
Stefano Mitello <stefano@mono.studio>
2022-08-12 13:09:36 +0200

    added object data

--- src/components/organisms/About/index.vue
+++ src/components/organisms/About/index.vue
@@ -7,14 +7,11 @@
         class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
       >
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          We are SpaceX {nome} <br class="hidden lg:inline-block" />MAKING
-          HUMANITY MULTIPLANETARY
+          We are {{ company.name }} <br class="hidden lg:inline-block" />
+          MAKING HUMANITY MULTIPLANETARY
         </h1>
         <p class="mb-8 leading-relaxed">
-          Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-          plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-          mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon
-          try-hard chambray. {citazione}
+          “{{ company.summary }}” -{{ company.founder }}
         </p>
         <div class="flex justify-center">
           <button
@@ -38,11 +35,35 @@
         />
       </div>
     </div>
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-wrap -m-4 text-center">
+        <Counter :to="company.employees" label="employees" />
+        <Counter :to="company.launch_sites" label="launch sites" />
+        <Counter :to="company.test_sites" label="test sites" />
+        <Counter :to="company.vehicles" label="vehicles" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import VideoWrapper from "../../atoms/VideoWrapper/index.vue";
+import Counter from "../../atoms/Counter/index.vue";
+
+export interface ICompany {
+  employees: number;
+  founder: string;
+  launch_sites: number;
+  name: string;
+  summary: string;
+  test_sites: number;
+  vehicles: number;
+}
+interface Props {
+  company: ICompany;
+}
+
+const props = defineProps<Props>();
 </script>
 
 <style scoped></style>
